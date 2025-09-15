28e68bb7c4c5875b85198bba445f745b47d12ff7
maria <westmoons7@gmail.com>
2025-09-15 19:59:01 +0300

    add task number 5

---
+++ semester_1/lab1_introduction/task-n-5/task-n-5/task-n-5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+int main() {
+	int n, m, min, i;
+	std::cout << "enter n = ";
+	std::cin >> n;
+	std::cout << "enter m = ";
+	std::cin >> m;
+	if (n <= 0 || m <= 0) {
+		std::cout << "eror 404";
+		return 0;
+	}
+
+
+
+	if (n < m) {
+		min = n;
+	}
+	else {
+		min = m;
+	}
+
+	for (i = 2; i <= min; i += 1) {
+		if (n % i == 0 && m % i == 0) {
+			std::cout << i <<" ";
+		}
+	}
+
+	return 0;
+}
