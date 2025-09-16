32c1c725b717b16c0cafbeacb853f817f3c4c424
maria <westmoons7@gmail.com>
2025-09-16 09:27:08 +0300

    add practic 16.09

---
+++ semester_1/lab2_integer_arithmetics/practic-16.09/practic-16.09/practic-16.09.cpp
@@ -0,0 +1,19 @@
+﻿//задание номер 6
+#include <iostream>
+#include <cmath>
+int main() {
+	int n, i=1, k=1;
+	std::cout << "enter a integer n = "; std::cin >> n;
+	while (n < 1) {
+		std::cout << "eror, enter an integer greater than zero n = "; std::cin>>n;
+	}
+	for (; k <= n;i += 1){
+		k = pow(2, i) - 1;
+		if (k > n) { return 0; }
+		std::cout << k << " ";
+	}
+	
+
+	
+	return 0;
+}
