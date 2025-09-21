4b39f0c804572cd371d5d626fcaafee956ebe828
maria <westmoons7@gmail.com>
2025-09-21 17:42:24 +0300

    added check

--- semester_1/lab2_integer_arithmetics/practic-16.09/practic-16.09/practic-16.09.cpp
+++ semester_1/lab2_integer_arithmetics/practic-16.09/practic-16.09/practic-16.09.cpp
@@ -1,19 +1,32 @@
 ﻿//задание номер 6
 #include <iostream>
 #include <cmath>
-int main() {
+
+int main() 
+{
 	int n, i=1, k=1;
 	std::cout << "enter a integer n = "; std::cin >> n;
-	while (n < 1) {
-		std::cout << "eror, enter an integer greater than zero n = "; std::cin>>n;
+	while (!(std::cin)||(n<=0))
+	{
+		if (!(std::cin)) 
+		{
+			std::cout << "Error is not a number";
+			break;
+		}
+		std::cout << "eror, enter an integer greater than zero n = "; 
+		std::cin >> n ;
 	}
-	for (; k <= n;i += 1){
+	for (; k <= n;i += 1)
+	{
 		k = pow(2, i) - 1;
-		if (k > n) { return 0; }
+		if (k > n) 
+		{
+			return 0;
+		}
 		std::cout << k << " ";
 	}
 	
-
+	
 	
 	return 0;
 }
