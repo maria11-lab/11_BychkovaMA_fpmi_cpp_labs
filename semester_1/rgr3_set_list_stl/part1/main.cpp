3625a28206c24e8c3489fba474b51aab1639929b
maria <westmoons7@gmail.com>
2025-12-22 20:08:00 +0300

    add 1 part

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,51 @@
+#include <iostream>
+#include <set>
+#include <cmath>
 
-
+void CheckInNum(int value) {
+    if (!(std::cin)) {
+        throw "Error! This is not a number\n";
+    }
+    if (value <= 0) {
+        throw "Error! This is a negative number!\n";
+    }
+}
 
 int main() {
+    try {
+        int n;
+        std::cout << "Enter n: ";
+        std::cin >> n;
+        CheckInNum(n);
+        if (n == 1) {
+            throw"There are no prime numbers less than 1";
+        }
 
-    // your code goes here
+        std::set<int> numbers;
+        for (int i = 2; i <= n; i++) {
+            numbers.insert(i);
+        }
 
-    return 0;
+        int limit = static_cast<int>(std::sqrt(n));
+
+        for (int p = 2; p <= limit; p++) {
+            if (numbers.find(p) != numbers.end()) {
+                for (int multiple = p * p; multiple <= n; multiple += p) {
+                    numbers.erase(multiple);
+                }
+            }
+        }
+
+        std::cout << "Prime numbers up to " << n << ":\n";
+        for (int prime : numbers) {
+            std::cout << prime << ' ';
+        }
+        std::cout << '\n';
+
+        return 0;
+    }
+    catch (const char* msg) {
+        std::cerr << msg;
+        return 1;
+    }
 }
